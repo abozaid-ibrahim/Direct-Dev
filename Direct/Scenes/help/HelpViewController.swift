1977a0d3831cdcf5c5499bffae4889ec05f63610
abuzeid ibrahim <abuzeid7@gmail.com>
2019-05-01 22:36:33 +0200

    help vc

---
+++ Direct/Scenes/help/HelpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HelpViewController.swift
+//  Direct
+//
+//  Created by abuzeid on 5/1/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+import UIKit
+import RxSwift
+import PanModal
+final class HelpViewController: UIViewController,PanModalPresentable {
+    var panScrollable: UIScrollView?{
+        return  tableView
+    }
+    @IBOutlet weak var tableView: UITableView!
+    private let disposeBag = DisposeBag()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerNib(SingleRowTableCell.cellId)
+        Observable<[String]>.just(dataList)
+            .bind(to: tableView.rx.items(cellIdentifier: SingleRowTableCell.cellId)) { row, model, cell  in
+                let mycell = (cell as! SingleRowTableCell)
+                mycell.setCellData((model))
+            }.disposed(by: disposeBag)
+        
+    }
+}
+
+
