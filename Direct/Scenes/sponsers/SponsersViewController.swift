5778aec42d2bc9b1404ca85ea8ac467ec49b5f16
abuzeid ibrahim <abuzeid7@gmail.com>
2019-05-17 19:23:30 +0200

    ui

--- Direct/Scenes/sponsers/SponsersViewController.swift
+++ Direct/Scenes/sponsers/SponsersViewController.swift
@@ -10,22 +10,39 @@ import UIKit
 
 class SponsersViewController: UIViewController {
     @IBOutlet var checkoutFooter: CheckoutFooter!
-
+    @IBOutlet private var tabbar: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "معلومات المتكفلين"
+        
         checkoutFooter.action = { [weak self] in
             try! AppNavigator().push(.passangersInfoScreen)
         }
+        setupTabbar()
     }
-
-
-    func setSponserName(){
+    
+    private func setupTabbar() {
+        let tab1 = ("المتكفل الأول ", { [weak self] in
+            
+        })
+        let tab2 = ("المتكفل الثاني ", { [weak self] in
+            
+        })
+        let tab3 = ("المتكفل الثالث ", { [weak self] in
+            
+        })
+        
+        let tabar = TabBar(tabs: [tab1, tab2, tab3])
+        tabar.addSubview(tabbar)
+        tabar.sameBoundsTo(parentView: tabbar)
+    }
+    
+    func setSponserName() {
         let attributedString = NSMutableAttributedString(string: "بيانات المتكفل بمصاريف المسافر ( عامر )", attributes: [
             .font: UIFont(name: "Cairo-Bold", size: 15.0)!,
             .foregroundColor: UIColor(white: 66.0 / 255.0, alpha: 1.0),
             .kern: 0.0
-            ])
+        ])
         attributedString.addAttribute(.foregroundColor, value: UIColor(red: 253.0 / 255.0, green: 133.0 / 255.0, blue: 13.0 / 255.0, alpha: 1.0), range: NSRange(location: 31, length: 8))
     }
 }
