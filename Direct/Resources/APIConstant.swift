ce4b0edefe14326bc78024b4e5b42de17a515779
abuzeid ibrahim <abuzeid7@gmail.com>
2019-06-14 18:06:15 +0200

    addign countries enum

--- Direct/Resources/APIConstant.swift
+++ Direct/Resources/APIConstant.swift
@@ -11,3 +11,35 @@ import Foundation
 enum APIConstant: String {
     case Key = "bf930e30-8c28-42d6-bf8e-f4cbd0b83774"
 }
+
+enum CountriesIDs: Int {
+    case US = 1,
+        GB = 2,
+        SGN = 3,
+        IN = 4,
+        CN = 5,
+        JP = 6,
+        IE = 7,
+        TR = 8
+
+    var endPointPath: String {
+        switch self {
+        case .US:
+            return "to-apply-us"
+        case .GB:
+            return "to-apply-gb"
+        case .SGN:
+            return "to-apply-sgn"
+        case .IN:
+            return "to-apply-in"
+        case .CN:
+            return "to-apply-cn"
+        case .JP:
+            return "to-apply-jp"
+        case .IE:
+            return "to-apply-ie"
+        case .TR:
+            return "to-apply-tr"
+        }
+    }
+}
